16b8b96e0ca466dc4345528bc885af69d47e4267
Johannes Opper <xijo@gmx.de>
2011-07-25 17:30:50 +0200

    Adds test mode for job scheduler

--- lib/job_scheduler.rb
+++ lib/job_scheduler.rb
@@ -1,11 +1,22 @@
 require 'resque'
 
 module JobScheduler
-  def self.schedule(job, *args)
+  @@test_mode = false
+
+  module_function
+
+  def test_mode(to = true)
+    @@test_mode = to
+  end
+
+  def schedule(job, *args)
     Module === job and job.ancestors.include?(Job) or
       raise TypeError, "#{job.inspect} is not a Job"
     if Resque.enqueue(job, *args)
       job
     end
+  rescue Errno::ECONNREFUSED => e
+    @@test_mode or raise e
   end
+
 end
