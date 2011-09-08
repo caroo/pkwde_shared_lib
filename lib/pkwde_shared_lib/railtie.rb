0d73a6e4f648aa95ad83a01a6fd724c33dde7f67
Johannes Opper <xijo@gmx.de>
2011-09-08 11:44:33 +0200

    Adds json serialization preferences to shared lib

--- lib/pkwde_shared_lib/railtie.rb
+++ lib/pkwde_shared_lib/railtie.rb
@@ -1,6 +1,9 @@
 module PkwdeSharedLib
   class Railtie < Rails::Railtie
     initializer "pkwde_shared_lib.require_service_extensions" do
+      JSON.create_id              = 'ruby_class'
+      ActiveSupport::JSON.backend = 'JSONGem'
+      require 'json/add/core'
       require 'pkwde/field_initialisation'
       require 'pkwde/json_serialisation'
       require 'job'
