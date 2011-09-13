df6f1f9b013676cf7f053a1f485a5fd997d4323e
Jens Fahnenbruck <jfahnenbruck@pkw.de>
2011-09-13 11:32:21 +0200

    removes json/add/core
    it's neccessary because of incompatibility with couch_potato

--- lib/pkwde_shared_lib/railtie.rb
+++ lib/pkwde_shared_lib/railtie.rb
@@ -3,7 +3,6 @@ module PkwdeSharedLib
     initializer "pkwde_shared_lib.require_service_extensions" do
       JSON.create_id              = 'ruby_class'
       ActiveSupport::JSON.backend = 'JSONGem'
-      require 'json/add/core'
       require 'pkwde/field_initialisation'
       require 'pkwde/json_serialisation'
       require 'job'
