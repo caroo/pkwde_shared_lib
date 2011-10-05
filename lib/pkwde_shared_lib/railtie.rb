96a294a35fca9c30c5b4993c9c5d2d2d4fceb68c
Florian Frank <flori@ping.de>
2011-10-05 15:42:06 +0200

    be more defensive while extending stuff

--- lib/pkwde_shared_lib/railtie.rb
+++ lib/pkwde_shared_lib/railtie.rb
@@ -2,12 +2,15 @@ module PkwdeSharedLib
   class Railtie < Rails::Railtie
     initializer "pkwde_shared_lib.require_service_extensions" do
       JSON.create_id              = 'ruby_class'
-      ActiveSupport::JSON.backend = 'JSONGem'
+      begin
+        ActiveSupport::JSON.backend = 'JSONGem'
+      rescue LoadError
+      end
       require 'pkwde/field_initialisation'
       require 'pkwde/json_serialisation'
       require 'job'
       require 'mail_interceptor'
-      Mail.register_interceptor(MailInterceptor)
+      defined?(Mail) and Mail.register_interceptor(MailInterceptor)
     end
   end
 end
