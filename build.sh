e610d523698559a7984a1d96bf9caf4a90a16c25
Florian Frank <flori@ping.de>
2012-03-13 11:33:09 +0100

    specific version

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@
 source "$HOME/.rvm/scripts/rvm"
 case $1 in
 *)
-  rvm ree@pkwde_shared_lib
+  rvm ree-1.8.7-2011.03@pkwde_shared_lib
   bundle install --binstubs
   bundle exec rake test
-  rvm 1.9.3@pkwde_shared_lib_1_9
+  rvm 1.9.3-p0@pkwde_shared_lib_1_9
   bundle install
   bundle exec rake test
 esac
